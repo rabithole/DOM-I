edc3c7dd3e2f5d6196281a13120ccdcd1813fc70
Robert Kulp <pedalinon@gmail.com>
2019-04-11 11:05:18 -0700

    Refactored using JSON data. Forgot to do that the first time around.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br> Is <br> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street <br> Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -37,88 +37,88 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+// Example: Update the img[counter] src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
 // Populate nav tags from siteContent object above. 
 let navItem = document.querySelectorAll('a');
+console.log(navItem)
 let navContent = Object.values(siteContent.nav)
 navContent.pop();
 // Learn to do this with forEach...
 let counter = 0;
 for(let i = 0; i < navContent.length; i++){
   navItem[counter].textContent = navContent[i];
+  navItem[counter].style.color = 'green';
   counter++;
 }
 
 // Header image
 let cta = document.querySelector('#cta-img')
-cta.src = 'img/header-img.png'
+cta.src = siteContent['cta']['img-src'];
 
 // Main H1
 let ctaH1 = document.querySelector('.cta-text h1')
-ctaH1.innerHTML = 'DOM <br> IS <br> AWESOME'
+ctaH1.innerHTML = siteContent['cta']['h1'];
 
 // Button after 'DOM IS AWESOME'
 let butTon = document.querySelector('button')
-butTon.textContent = 'Get Started';
+butTon.textContent = siteContent['cta']['button'];
 
 // class top-content, first H4
 let topContent = document.querySelector('.top-content div:nth-child(1) h4')
-topContent.textContent = 'FEATURES';
-// Selector for first h4 body > div > section.main-content > div.top-content > div:nth-child(1) > h4
+topContent.textContent = siteContent['main-content']['features-h4'];
+
 let firstP = document.querySelector('.top-content div:nth-child(1) p')
-firstP.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+firstP.textContent = siteContent['main-content']['features-content']
 
 
 // class top-content, second H4
 let topP = document.querySelector('.top-content div:nth-child(2) h4')
-topP.textContent = 'ABOUT';
+topP.textContent = siteContent['main-content']['about-h4'];
 // Selector for second H4 body > div > section.main-content > div.top-content > div:nth-child(2) > h4
 let secondP = document.querySelector('.top-content div:nth-child(2) p')
-secondP.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+secondP.textContent = siteContent['main-content']['about-content'];
 // selector for second paragraph body > div > section.main-content > div.top-content > div:nth-child(2) > p
 
 
 let middleImage = document.querySelector('.middle-img');
-middleImage.src = 'img/mid-page-accent.jpg';
+middleImage.src = siteContent['main-content']['middle-img-src'];
 
 let bottomContentH4_1 = document.querySelector('.bottom-content div:nth-child(1) h4')
-bottomContentH4_1.textContent = 'SERVICES';
+bottomContentH4_1.textContent = siteContent['main-content']['services-h4'];
 
 let bottomContentH4_2 = document.querySelector('.bottom-content div:nth-child(2) h4')
-bottomContentH4_2.textContent = 'PRODUCT';
+bottomContentH4_2.textContent = siteContent['main-content']['services-h4'];
 
 let bottomContentH4_3 = document.querySelector('.bottom-content div:nth-child(3) h4')
-bottomContentH4_3.textContent = 'VISON';
+bottomContentH4_3.textContent = siteContent['main-content']['vision-h4'];
 
 let bottomContentP_1 = document.querySelector('.bottom-content div:nth-child(1) p')
-bottomContentP_1.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomContentP_1.textContent = siteContent['main-content']['services-content'];
 
 let bottomContentP_2 = document.querySelector('.bottom-content div:nth-child(2) p')
-bottomContentP_2.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomContentP_2.textContent = siteContent['main-content']['product-content'];
 
 let bottomContentP_3 = document.querySelector('.bottom-content div:nth-child(3) p')
-bottomContentP_3.textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomContentP_3.textContent = siteContent['main-content']['vision-content'];
 
 let contactH4 = document.querySelector('.contact h4')
-contactH4.textContent = 'contact';
+contactH4.textContent = siteContent['contact']['contact-h4'];
 
 let contactP_2 = document.querySelector('.contact p:nth-child(2)')
-contactP_2.innerHTML = '123 Way 456 Street <br> Somewhere, USA';
+contactP_2.innerHTML = siteContent['contact']['address'];
 
 let contactP_3 = document.querySelector('.contact p:nth-child(3)')
-contactP_3.innerHTML = '1 (888) 888-8888';
+contactP_3.innerHTML = siteContent['contact']['phone'];
 
 let contactP_4 = document.querySelector('.contact p:nth-child(4)')
-contactP_4.innerHTML = 'sales@greatidea.io';
+contactP_4.innerHTML = siteContent['contact']['email'];
 
 let footerP = document.querySelector('footer p')
-footerP.textContent = 'Copyright Great Idea! 2018'
+footerP.textContent = siteContent['footer']['copyright'];
 
-// body > div > section.contact > p:nth-child(2)
 
-console.log(contactP_1)
 
